9c68a35e0d6300dbbcc77a3f404a1767c7f81285
Christian Walde <walde.christian@googlemail.com>
2012-03-31 17:41:46 +0200

    implementation with directory walking instead of komodo parameter

--- lib/Perl/Tidy/Komodo.pm
+++ lib/Perl/Tidy/Komodo.pm
@@ -4,8 +4,40 @@ package Perl::Tidy::Komodo;
 
 # VERSION
 
-# ABSTRACT:
+# ABSTRACT: tidy perl files in Komodo with a project rc
 
 # COPYRIGHT
+
+use Perl::Tidy;
+use File::chdir;
+
+=head1 METHODS
+
+=head2 run
+
+Tries to find a directory that looks like a project directory, upwards
+from CWD and stores its path in $ENV{PERLTIDY}. Then Perl::Tidy itself is
+run.
+
+=cut
+
+sub run {
+    _try_set_perltidy_env();
+    Perl::Tidy::perltidy();
+}
+
+sub _try_set_perltidy_env {
+    return if exists $ENV{PERLTIDY};
+    my @cwd_store = @CWD;
+    while ( $CWD[0] ) {
+        if ( -d 'lib' and -f '.perltidyrc' ) {
+            $ENV{PERLTIDY} = $CWD;
+            last;
+        }
+        pop @CWD;
+    }
+    @CWD = @cwd_store;
+    return;
+}
 
 1;
